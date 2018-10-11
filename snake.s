3f5f88452c05a71ae355f844f1984607ac9c80a9
konsta <konsta@gamedev.delftgaming>
2018-10-11 15:34:02 +0200

    Data structure documentation

--- snake.s
+++ snake.s
@@ -1,18 +1,37 @@
-.text
+section .data
+# starting position: third row, third column, three-block snake
+starthead: .byte 765
+startlength: .byte 3
 
+section .bss
+# screen:
+gamearea: .skip 4032
+# maximum length of snake is the same as game area, queue needs +1:
+snakequeue: .skip 365
+
+
+section .text
 
 .globl main
-
 
 main:
     pushq   %rbp
     movq    %rsp, %rbp
 
+    initgamearea
+    initsnake
+
     # prepare to init everything
     movl	$29233, %edi
-        
     # init call
     call	SDL_Init
+
+    #
+    # draw stuff on canvas here
+    #
+
+
+
 
     # shut down sdl
     call    SDL_Quit
@@ -20,3 +39,12 @@ main:
 
     movq    $0, %rdi
     call    exit
+
+
+.macro initgamearea
+
+.endm
+
+.macro initsnake
+
+.endm
