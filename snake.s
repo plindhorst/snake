25f7540e8a280f1fa257a09ac88e906cd5a3c87d
konsta <konsta@gamedev.delftgaming>
2018-10-15 16:48:32 +0200

    Create window with SDL

--- snake.s
+++ snake.s
@@ -1,115 +1,152 @@
+# Variables
 .data
-# starting position: third row, third column, three-block snake
-starthead: .value 765
-startlength: .value 3
 
+
+# Zero-initialized memory areas
 .bss
-# screen:
-gamearea: .skip 4032
-# maximum length of snake is the same as game area, queue needs +1:
-snakequeue: .skip 365
+    # Screen:
+    gamearea: .skip 4032
+    # Maximum length of snake is the same as game area, queue needs +1:
+    snakequeue: .skip 365
 
 
+# Constants
 .text
+    # Starting position: third row, third column, three-block snake
+    starthead: .value 765
+    startlength: .value 3
 
-timeprint:  .asciz "%ld\n"
-
-.globl main
+    # Window
+    window_title: .asciz "Snake"           # window title
+    .equ window_height, 384                 # window height is 384
+    .equ window_width, 669                  # window width is 672
+    .equ window_x, 536805376                # undefined
+    .equ window_y, 536805376                # undefined
+    
 
 
 .macro INIT_GAME_AREA
-    # build walls
+        # Build walls
 
-    # top row:
-    # for i = 85 to 166 (inclusive), write 1 to gamearea[i]
-    movq    $85, %rbx
-    movq    $166, %rcx
-top_loop:
-    movb    $1, gamearea(%rbx)
-    incq    %rbx
-    cmpq    %rcx, %rbx
-    jle     top_loop
+        # Top row:
+        # for i = 85 to 166 (inclusive), write 1 to gamearea[i]
+        movq    $85, %rbx
+        movq    $166, %rcx
+    top_loop:
+        movb    $1, gamearea(%rbx)
+        incq    %rbx
+        cmpq    %rcx, %rbx
+        jle     top_loop
 
-    # bottom row:
-    # for i = 3865 to 3946 (inclusive)
-    movq    $3865, %rbx
-    movq    $3946, %rcx
-bottom_loop:
-    movb    $1, gamearea(%rbx)
-    incq    %rbx
-    cmpq    %rcx, %rbx
-    jle     bottom_loop
+        # Bottom row:
+        # for i = 3865 to 3946 (inclusive)
+        movq    $3865, %rbx
+        movq    $3946, %rcx
+    bottom_loop:
+        movb    $1, gamearea(%rbx)
+        incq    %rbx
+        cmpq    %rcx, %rbx
+        jle     bottom_loop
 
-    # left column:
-    # i = 169, for 44 times, increment by 84
-    movq    $169, %rbx
-    movq    $44, %rcx
-left_loop:
-    movb    $1, gamearea(%rbx)
-    addq    $84, %rbx
-    decq    %rcx
-    cmpq    $0, %rcx
-    jg      left_loop
+        # Left column:
+        # i = 169, for 44 times, increment by 84
+        movq    $169, %rbx
+        movq    $44, %rcx
+    left_loop:
+        movb    $1, gamearea(%rbx)
+        addq    $84, %rbx
+        decq    %rcx
+        cmpq    $0, %rcx
+        jg      left_loop
 
-    # right column:
-    # i = 250, for 44 times, increment by 84
-    movq    $250, %rbx
-    movq    $44, %rcx
-right_loop:
-    movb    $1, gamearea(%rbx)
-    addq    $84, %rbx
-    decq    %rcx
-    cmpq    $0, %rcx
-    jg      right_loop
+        # Right column:
+        # i = 250, for 44 times, increment by 84
+        movq    $250, %rbx
+        movq    $44, %rcx
+    right_loop:
+        movb    $1, gamearea(%rbx)
+        addq    $84, %rbx
+        decq    %rcx
+        cmpq    $0, %rcx
+        jg      right_loop
 
 .endm
+
 
 .macro INIT_SNAKE
-
 .endm
 
+
 .macro DISPATCH_APPLE
-    # here we need some randomness.
-    # apple can be on any block on game area that is not a snake block
+    # Here we need some randomness.
+    # Apple can be on any block on game area that is not a snake block
     call    clock_gettime
     movq    %r9, %rax
     xor     %rdx, %rdx
     movq    $10, %rcx
     divq    %rcx
-
-    movq    %rdx, %rsi
-    movq    $timeprint, %rdi
-    call    printf
+    # A random number 0 <= r <= 9 is in rdx now
 
 .endm
 
 
 
-
+.globl main
 
 main:
+    # Init basepointer
     pushq   %rbp
     movq    %rsp, %rbp
 
-    INIT_GAME_AREA
-    INIT_SNAKE
-    DISPATCH_APPLE
-
-    # prepare to init everything
-    movl	$29233, %edi
-    # init SDL
+    # SDL_INIT_EVERYTHING = 0x7231
+    movl	$0x7231, %edi
+    # Init SDL
     call	SDL_Init
 
+    # Create window:
+    # Parameters go to rdi, rsi, rdx, rcx, r8 and r9;
+    # SDL_CreateWindow wants window title, x, y, width, height and flags
+    # SDL_WINDOW_OPENGL = 0x2
+    movq    $window_title, %rdi
+    movl    $window_x, %esi
+    movl    $window_y, %edx
+    movl    $window_width, %ecx
+    movl    $window_height, %r8d
+    movl    $0x2, %r9d
+    call    SDL_CreateWindow
+    # This returns a pointer to a window. Save it in r12 for use:
+    movq    %rax, %r12
+
+
+
+
+
+
+
+    #INIT_GAME_AREA
+    #INIT_SNAKE
+    #DISPATCH_APPLE
+
     #
-    # draw stuff on canvas here
+    # Draw stuff on canvas here
     #
 
 
 
 
-    # shut down SDL
+end:
+    # For testing: wait for n loops
+    incq    %rbx
+    # 10^9 makes for a noticeable delay
+    cmpq    $2000000000, %rbx
+    jl      end
+
+
+    # Destroy window
+    movq    %r12, %rdi
+    call    SDL_DestroyWindow
+    # Shut down SDL
     call    SDL_Quit
-
-
+    # Exit code 0
     movq    $0, %rdi
     call    exit
